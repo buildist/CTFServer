9046ee15b2bf3d8ed1658108405bf560108b5f74
dakriy <git@persignum.com>
2021-06-22 12:05:11 -0700

    Add discord command to query the server who is currently on the server

--- src/main/java/org/opencraft/server/DiscordBot.java
+++ src/main/java/org/opencraft/server/DiscordBot.java
@@ -1,7 +1,9 @@
 package org.opencraft.server;
 
+import java.util.List;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
+import org.opencraft.server.model.Player;
 import org.opencraft.server.model.World;
 
 public class DiscordBot implements Runnable {
@@ -18,13 +20,27 @@ public class DiscordBot implements Runnable {
         .setToken(Configuration.getConfiguration().getDiscordToken()).login().join();
     api.addMessageCreateListener(event -> {
       if (event.getMessageAuthor().isYourself() || event.getMessageAuthor().isWebhook()
-          || event.getChannel().getId() != CHANNEL_ID  || event.getMessage().getContent().isBlank()) {
+          || event.getChannel().getId() != CHANNEL_ID || event.getMessage().getContent()
+          .isBlank()) {
         return;
       }
+      String message = event.getMessage().getContent();
       System.err.println("[Discord] " + event.getMessageAuthor().getDisplayName()
-          + ": " + event.getMessage().getContent());
-      World.getWorld().broadcast("&5[Discord] &f" + event.getMessageAuthor().getDisplayName()
-          + ": " + sanitizeDiscordInput(event.getMessage().getContent()));
+          + ": " + message);
+      switch (message) {
+        case ".who", ".players" -> {
+          List<Player> players = World.getWorld().getPlayerList().getPlayers();
+          StringBuilder messageBuilder = new StringBuilder();
+          messageBuilder.append("Players:");
+          for (Player p : players) {
+            messageBuilder.append(" ").append(p.getName());
+          }
+          event.getChannel().sendMessage(messageBuilder.toString());
+        }
+        default -> World.getWorld()
+            .broadcast("&5[Discord] &f" + event.getMessageAuthor().getDisplayName()
+                + ": " + sanitizeDiscordInput(message));
+      }
     });
   }
 }
