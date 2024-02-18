5ba58ba50b053b4c170e7c66153788c320d7586a
Jacob Morgan <jacobmorgan715@gmail.com>
2024-02-18 10:39:49 -0800

    use latest Javacord from git

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,14 +13,42 @@ dependencies {
     implementation(libs.guava)
     implementation(libs.mina)
     implementation(libs.xstream)
-    implementation(libs.javacord)
     implementation(libs.jskills)
     implementation(fileTree(mapOf("dir" to "lib", "include" to listOf("*.jar"))))
 
     testImplementation(libs.junit)
 
     runtimeOnly(libs.slf4jSimple)
-}
+    // Javacord dependencies below
+
+    // OkHttp for REST-calls
+    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.9.3"))
+
+    // define any required OkHttp artifacts without version
+    implementation("com.squareup.okhttp3:okhttp")
+    implementation("com.squareup.okhttp3:logging-interceptor")
+
+    // the JSON-lib because Discord returns in JSON format
+    // Update when upgrading to Java 11+
+    // https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-databind
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.12.7.1")
+
+    // the web socket
+    implementation("com.neovisionaries:nv-websocket-client:2.14")
+
+    // voice encryption
+    implementation("com.codahale:xsalsa20poly1305:0.11.0")
+
+    // logging
+    implementation("org.apache.logging.log4j:log4j-api:2.17.2")
+
+    // Vavr, mainly for immutable collections
+    // We are using 0.10.1, because of an issue in 0.10.2: https://github.com/vavr-io/vavr/issues/2573
+    implementation("io.vavr:vavr:0.10.4")
+
+    // For old @Generated annotation in Java 9
+    // can be replaced by javax.annotation.processing.Generated if Java 9 is minimum requirement
+    compileOnly("javax.annotation:javax.annotation-api:1.3.2")}
 
 val mainClassName = "org.opencraft.server.Server"
 
