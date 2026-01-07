0517c2c9a4d90f62c9a1b66ae4df3fd639ba5ba5
dakriy <github@persignum.com>
2026-01-07 15:48:49 -0800

    fix: I forgot a single character, and it broke grenades, this fixes grenades

--- src/main/java/org/opencraft/server/game/impl/CTFGameMode.java
+++ src/main/java/org/opencraft/server/game/impl/CTFGameMode.java
@@ -177,7 +177,7 @@ public class CTFGameMode extends GameMode {
     if (lethal) {
       float px = x + 0.5f, py = y + 0.5f, pz = z + 0.5f;
       float pr = r + 0.5f;
-      float prv = r;
+      float prv = pr;
 
       if (extendedVerticalRange) {
         prv += PLAYER_POSITION_OFFSET;
